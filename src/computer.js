6b0a3c66dd83713c9ab2b09488a44482a4c68857
Mudi Ugbowanko <mudi@renegare.com>
2017-06-23 20:40:22 +0100

    implement computer class

--- src/computer.js
+++ src/computer.js
@@ -1,6 +1,11 @@
-export const Computer = {
-  play: () => {
+class Computer {
+  constructor(possibleOptions) {
+    this.possibleOptions = possibleOptions
+  }
 
+  play (setChoice) {
+    const idx = Math.floor(Math.random() * this.possibleOptions.length)
+    setChoice(this.possibleOptions[idx])
   }
 }
 
---
+++ test/computer.spec.js
@@ -0,0 +1,38 @@
+import Computer from '../src/computer'
+import { stub } from 'sinon'
+
+describe('the computer', () => {
+  let random
+  let floor
+
+  before(() => {
+    random = stub(Math, 'random')
+    floor = stub(Math, 'floor')
+  })
+
+  beforeEach(() => {
+    random.reset()
+    floor.reset()
+  })
+
+  after(() => {
+    random.restore()
+    floor.restore()
+  })
+
+  it.only('should return a random choice', done => {
+    const possibleOptions = [ 1, 2, 3 ]
+    const computer = new Computer(possibleOptions)
+    random.returns(0.5)
+    floor.returns(2)
+
+    computer.play(choice => {
+      expect(choice).to.eql(3)
+
+      expect(random.calledOnce).to.be.true
+      expect(floor.calledOnce).to.be.true
+
+      done()
+    })
+  })
+})
