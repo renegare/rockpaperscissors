de5321ec8b6eee4e27deab7fec31cdd9aab9259b
Mudi Ugbowanko <mudi@renegare.com>
2017-06-26 16:46:04 +0100

    split initial test

--- test/components/game-screen.spec.js
+++ test/components/game-screen.spec.js
@@ -16,6 +16,8 @@ describe.only('Game Screen', () => {
   beforeEach(() => {
     create.reset()
     getOptions.reset()
+
+    create.callsFake((...args) => new Game(...args))
   })
 
   after(() => {
@@ -23,10 +25,9 @@ describe.only('Game Screen', () => {
     getOptions.restore()
   })
 
-  it('should display options to choose from', done => {
+  it('should display options to choose from', () => {
     const expectedOptions = [ 'ROCK', 'PAPER', 'SCISSORS' ]
     getOptions.returns(expectedOptions)
-    create.callsFake((...args) => new Game(...args))
 
     const wrapper = shallow(<GameScreen />)
     expect(wrapper.find('button')).to.length(0)
@@ -36,21 +37,34 @@ describe.only('Game Screen', () => {
     const componentPlayHandler = create.firstCall.args[0]
 
     // simulate game calling for the users selection
-    componentPlayHandler(selectedOption => {
-      // verify game is passed the user's selection
-      expect(selectedOption).to.eql(expectedOptions[1])
-      // after user selection the options should no longer be available
-      // expect(wrapper.find('button')).length(0)
-      done()
-    })
+    componentPlayHandler(() => {})
 
     // verify options have been rendered
     const buttons = wrapper.find('button')
     const options = buttons.map(n => n.text())
     expect(buttons).to.have.length(expectedOptions.length)
     expect(options).to.eql(expectedOptions)
+  })
 
-    // user plays
+  it('should disable options after user has made a selection', done => {
+    const expectedOptions = [ 'ROCK', 'PAPER', 'SCISSORS' ]
+    getOptions.returns(expectedOptions)
+    create.callsFake((...args) => new Game(...args))
+
+    const wrapper = shallow(<GameScreen />)
+    expect(wrapper.find('button')).to.length(0)
+
+    create.firstCall.args[0](selectedOption => {
+      expect(selectedOption).to.eql(expectedOptions[1])
+
+      setTimeout(() => {
+        expect(wrapper.find('button')).length(0)
+        done()
+      })
+    })
+
+    const buttons = wrapper.find('button')
+    expect(buttons).length(3)
     buttons.at(1).simulate('click')
   })
 })
